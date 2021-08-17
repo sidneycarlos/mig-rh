8ce33ed75dea87b8f7ea4fb966614118b255acae
sidney_carlos <bwb.carlos.s@fondespierre.com>
2021-08-17 16:57:06 +0000

    templating

---
+++ src/app/template/template.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TemplateModule { }
