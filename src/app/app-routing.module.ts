7072dccfcc67142ed4c1ff1459c490433a8e0e8d
sidney_carlos <bwb.carlos.s@fondespierre.com>
2021-08-18 04:38:58 +0000

    routing set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { TeamsComponent } from './pages/teams/teams.component';
+import { OperationsComponent } from './pages/operations/operations.component';
+import { AllstaffComponent } from './pages/allstaff/allstaff.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EmployeesComponent } from './pages/employees/employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "dashboard", component: DashboardComponent
+  },
+  {
+    path: "employees", component: EmployeesComponent
+  },
+  {
+    path: "allstaff", component: AllstaffComponent
+  },
+  {
+    path: "operations", component: OperationsComponent
+  },
+  {
+    path: "teams", component: TeamsComponent
+  }
+] as Routes;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
